4057617664db7627a87c96ebb5a15c6aa1aaf457
darkMasterv <79690406+lightdarkmaster@users.noreply.github.com>
2025-02-04 21:48:57 +0800

    Add Tax

--- functions.py
+++ functions.py
@@ -1,6 +1,11 @@
+
 def addFunctions():
     num1 = int(input('Enter Num1'));
     num2 = int(input('Enter Num2'));
     
     sumOfNumbers = num1 + num2;
     print(sumOfNumbers);
+    
+    total_cost = sumOfNumbers * .010;
+    print(total_cost);
+    
