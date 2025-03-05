560f61947d705734f8896481eece5d670d7baf3c
darkMasterv <79690406+lightdarkmaster@users.noreply.github.com>
2025-03-06 07:32:18 +0800

    Accept SIngle input only

--- scripts.js
+++ scripts.js
@@ -23,11 +23,51 @@ console.log(test2([1,2,3,4,5,6]));
 
 
 const ktest =(value)=>{
-    let result = 0;
-    for (let i = 0; i < value; i++) {
-        result += value;
-    }
-    return result;
+    let result = [];
+    return array.map(function(x){
+        for(let i = 0; i < value; i++){
+            result += value;
+        }
+        return result
+    })
+
+    //for (let i = 0; i < value; i++) {
+        //result += value;
+    //}
+    //return result;
 }
 
-console.log(ktest(4));
+//console.log(ktest(4));
+
+
+
+const f =(array,length)=>{
+
+    let s = [];
+    for(let i; i < array; i++){
+        
+    }
+}
+
+//
+
+
+const solved =()=>{
+    let given = 2;
+    let given2 = 2;
+    
+    //default
+    const num1 = 5;//multiply to the first number
+    const num2 = 8;//add 
+    const num3 = 2;//minus prev operation;
+
+    const o1 = given * num1;
+    const o2 = o1 + num2;
+    const o3 = o2 * given2;
+    const o4 = o3 - num3;
+    
+    return o4;
+
+
+}
+console.log(solved())
